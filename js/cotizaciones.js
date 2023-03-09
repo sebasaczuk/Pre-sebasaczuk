006345e2533463d1b398d915f93755ec5a18d2fd
sebasaczuk <seba.saczuk@gmail.com>
2023-03-09 15:48:51 -0300

    mas ajustes

--- js/cotizaciones.js
+++ js/cotizaciones.js
@@ -1,7 +1,7 @@
 //Cotizador de estudios y practicas, primera entrega curso Coder. Sebastian Saczuk.
 let consulta = confirm("¿Te puedo ayudar a cotizar tu practica?")
 if (consulta === false) {
-    location.href = '/index.html'
+    location.href = '../index.html'
 }
 
 if (consulta === true) {
@@ -11,30 +11,30 @@ if (consulta === true) {
         case "radiografias":
             costos1 = 100
         calcularImporteFinal()  
-        location.href = '/index.html'
+        location.href = '../index.html'
         //cotizo Radiografias
             break
         case "tomografias":
             costos1 = 200
         calcularImporteFinal()
-        location.href = '/index.html'
+        location.href = '../index.html'
         //cotizo Tomografias
             break
         case "extracciones":
             costos1 = 300
         calcularImporteFinal()
-        location.href = '/index.html'
+        location.href = '../index.html'
         //cotizo Extracciones
             break
         case "resonancias":
             costos1 = 400
         calcularImporteFinal()
-        location.href = '/index.html'
+        location.href = '../index.html'
         //cotizo extracciones
             break
         default:
             //sino hago el estudio...
         alert("⛔ No realizamos el estudio/practica", estudio)
-        location.href = '/index.html'
+        location.href = '../index.html'
         }  
 }
